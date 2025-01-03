a5b60436b69b20262ff08033c63e743b761cd776
Amanofficial07 <136831534+Amanofficial07@users.noreply.github.com>
2025-01-03 23:33:09 +0530

    Create jdbc.java

---
+++ Web/server/jdbc.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+public class Jdbc {
+
+	public static void main(String[] args) {
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:4214/tree","root", "Aman07$");
+		    Statement stmt = con.createStatement();
+		    System.out.println("Inserting Records");
+		    String sql = "Insert into fruit value(54)";
+		    stmt.executeUpdate(sql);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+
+}
